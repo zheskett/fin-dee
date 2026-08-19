5f0f3cda4746e8635466717fb27984b1319d4ffc
Zachary Heskett <zheskett@gmail.com>
2026-08-18 22:17:19 -0200

    finished simplefin api

--- src/main/kotlin/common/SimpleFinObjects.kt
+++ src/main/kotlin/common/SimpleFinObjects.kt
@@ -1,8 +1,52 @@
 package findee.common
 
-import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.*
 import kotlinx.serialization.json.*
+import io.ktor.serialization.kotlinx.json.*
 
 @Serializable
-data class SimpleFinAccount(val id: String, val name: String, @SerialName("conn_id") val connId: String)
+data class SimpleFinError(
+    val code: String,
+    val msg: String,
+    @SerialName("conn_id") val connId: String? = null,
+    @SerialName("account_id") val accountId: String? = null,
+)
+
+@Serializable
+data class SimpleFinConnection(
+    @SerialName("conn_id") val connId: String,
+    val name: String,
+    @SerialName("org_id") val orgId: String,
+    @SerialName("org_url") val orgUrl: String? = null,
+    @SerialName("sfin_url") val sfinUrl: String,
+)
+
+@Serializable
+data class SimpleFinAccountSet(
+    val errlist: List<SimpleFinError>,
+    val connections: List<SimpleFinConnection>,
+    val accounts: List<SimpleFinAccount>,
+)
+
+@Serializable
+data class SimpleFinAccount(
+    val id: String,
+    val name: String,
+    @SerialName("conn_id") val connId: String,
+    val currency: String,
+    val balance: String,
+    @SerialName("available-balance") val availableBalance: String? = null,
+    @SerialName("balance-date") val balanceDate: Long,
+    val transactions: List<SimpleFinTransaction>? = null,
+    // extra ignored
+)
+
+@Serializable
+data class SimpleFinTransaction(
+    val id: String,
+    val posted: Long,
+    val amount: String,
+    val description: String,
+    @SerialName("transacted_at") val transactedAt: Long? = null,
+    val pending: Boolean = false,
+)
